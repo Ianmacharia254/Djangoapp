3bdb38fd5392cb52a84a136a7a37c819f20703e8
Ianmacharia254 <machariaian65@gmail.com>
2024-07-17 00:33:38 +0300

    python

--- Djangoapp/views.py
+++ Djangoapp/views.py
@@ -19,7 +19,7 @@ def product_detail(request, pk):
 def customer_list(request):
     customers = customer.objects.all()
     context ={
-        'customer': customers
+        'customers': customers
     }
     return render(request, 'Djangoapp/customer_list.html', context)
 
@@ -29,3 +29,13 @@ def customer_detail(request, pk):
          'customer': customer
      }
      return render(request,'Djangoappp/customer_detail.html', context)
+ 
+     from django.shortcuts import render
+
+from django.shortcuts import render
+
+def home(request):
+    context = {
+        'message': 'Welcome to the Home Page!'
+    }
+    return render(request, 'Djangoapp/home.html', context)
