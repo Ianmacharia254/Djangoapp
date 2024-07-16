f44375431a13924c4e9489e00c8678641fabdc15
Ianmacharia254 <machariaian65@gmail.com>
2024-07-16 14:56:40 +0300

    updated files for Djangoapp

--- Djangoapp/models.py
+++ Djangoapp/models.py
@@ -1,4 +1,29 @@
 from django.db import models
 
 # Create your models here.
-
+class product(models.Model):
+    name = models.CharField(max_length=300)
+    description =models.TextField()
+    price =models.DecimalField(max_digits=10,decimal_places=2)
+    image = models.ImageField(upload_to='products/')
+    Availability = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
+    
+class customer(models.Model):
+        first_name = models.CharField(max_length=60)
+        last_name = models.CharField(max_length=60)
+        email = models.EmailField(unique=True)
+        phone =models.CharField(max_length=20, blank=True, null=True)
+        address =models.TextField(blank=True, null=True)
+        created_at = models.DateTimeField(auto_now_add=True)
+        
+        def __str__(self):
+            return f"{self.first_name} {self.last_name} {self.email}"
+    
+    
+class Details(models.Model):
+        pass
+    
+                               
--- Djangoapp/views.py
+++ Djangoapp/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import product, customer
 
 # Create your views here.
-def home(request):
-    return HttpResponse("<h1 style= 'color: purple'>our web progress<h1>")
+def product_list(request):
+    products = product.objects.all()
+    context ={
+        'products': products,
+    }
+    return render(request,'Djangoapp/product_list.html', context)
+
+def product_detail(request, pk):
+    product = get_object_or_404(product, pk=pk)
+    context ={
+        'products': product,
+    }
+    return render(request, 'Djangoapp/product_details.html', context)
+
+def customer_list(request):
+    customers = customer.objects.all()
+    context ={
+        'customer': customers
+    }
+    return render(request, 'Djangoapp/customer_list.html', context)
+
+def customer_detail(request, pk):
+     customer = get_object_or_404(customer, pk=pk)
+     context = {
+         'customer': customer
+     }
+     return render(request,'Djangoappp/customer_detail.html', context)
